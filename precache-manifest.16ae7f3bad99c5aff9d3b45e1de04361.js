79d4415bd150315b22bce4a9ca92a7ad589aaa5b
nhaouari <nhaouari@users.noreply.github.com>
2018-11-09 18:27:35 +0100

    sooversion 9

--- precache-manifest.16ae7f3bad99c5aff9d3b45e1de04361.js
+++ precache-manifest.16ae7f3bad99c5aff9d3b45e1de04361.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/CRATE/static/js/runtime~main.e2f9dc5b.js"
   },
   {
-    "revision": "0b6c33c1fcdcb85a754a",
-    "url": "/CRATE/static/js/main.0b6c33c1.chunk.js"
+    "revision": "0d489bd18b520844dbff",
+    "url": "/CRATE/static/js/main.0d489bd1.chunk.js"
   },
   {
     "revision": "71e21854368244ca7a1c",
     "url": "/CRATE/static/js/1.71e21854.chunk.js"
   },
   {
-    "revision": "0b6c33c1fcdcb85a754a",
+    "revision": "0d489bd18b520844dbff",
     "url": "/CRATE/static/css/main.e357cc70.chunk.css"
   },
   {
-    "revision": "c475636b082539226f6b391778971020",
+    "revision": "4fa2d6b2e462fe339682436c0edc7bef",
     "url": "/CRATE/index.html"
   }
 ];
